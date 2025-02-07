af66b9f11cbe4dc060ce1b9bed904f61f0679a0a
shogo4131 <shougo08697118@gmail.com>
2025-02-07 17:25:48 +0900

    2/7の学習

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,101 @@
-import Image from "next/image";
+"use client";
+
+import { useActionState } from "react";
+import { useFormStatus } from "react-dom";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  // const [name, setName] = useState("");
+  // const [error, setError] = useState(null);
+  // const [isPending, setIsPending] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  // const handleSubmit = async () => {
+  //   console.log("handleSubmit");
+
+  //   setIsPending(true);
+  //   const error = await updateName(name);
+  //   setIsPending(false);
+  //   if (error) {
+  //     setError(error as unknown as string);
+  //     return;
+  //   }
+  //   redirect("/path");
+  // };
+
+  // return (
+  //   <div className="">
+  //     <input
+  //       className="bg-black text-white"
+  //       value={name}
+  //       onChange={(event) => setName(event.target.value)}
+  //     />
+  //     <button
+  //       className="border-1 border-black text-black"
+  //       onClick={handleSubmit}
+  //       disabled={isPending}
+  //     >
+  //       Update
+  //     </button>
+  //     {error && <p>{error}</p>}
+  //   </div>
+  // );
+
+  /**
+   * onChangeとか使わずにReactのAPIだけでform送信が完結する
+   * バリデーションがclientではなくserver側で実行しないといけない？
+   */
+  const { pending } = useFormStatus();
+
+  const [error, submitAction, isPending] = useActionState(
+    async (previousState, formData) => {
+      const error = await updateName(formData.get("name") as string);
+      if (error) {
+        return error;
+      }
+      return null;
+    },
+    null
   );
+  // console.log("isPending", isPending);
+
+  console.log("useFormStatus", pending);
+
+  return (
+    <form action={submitAction}>
+      <input className="bg-black text-white" name="name" />
+      <FromButton>Update</FromButton>
+      {error && <p>{error}</p>}
+    </form>
+  );
+}
+
+const FromButton = ({ children }: { children: React.ReactNode }) => {
+  const { pending, method, action } = useFormStatus();
+  // true
+  console.log("pending", pending);
+  console.log("method", method);
+  console.log("action", action);
+  return <Button disabled={pending}>{children}</Button>;
+};
+
+// ui ボタン
+const Button = ({
+  children,
+  disabled,
+}: {
+  children: React.ReactNode;
+  disabled: boolean;
+}) => {
+  return (
+    <button type="submit" disabled={disabled}>
+      {children}
+    </button>
+  );
+};
+
+async function updateName(name: string) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(name);
+    }, 1000);
+  });
 }
